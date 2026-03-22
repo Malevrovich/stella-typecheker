d8100f5697736124996d74345aac7352dc164389
Ivan Skriabin <dddccc20000@mail.ru>
2026-03-22 21:23:32 +0300

    TypeChecker trySetDeducedTypeFamily

--- typechecker/checker/include/stella/typecheck/type_checker.hpp
+++ typechecker/checker/include/stella/typecheck/type_checker.hpp
@@ -7,6 +7,8 @@
 #include "stella/typecheck/error.hpp"
 #include "stella/typecheck/expected_type.hpp"
 #include "stella/typecheck/name_context.hpp"
+#include <concepts>
+#include <type_traits>
 
 namespace stella {
 namespace typecheck {
@@ -31,12 +33,25 @@ private:
 
     void ExpectType(const ast::NodeBase& node, ExpectedType&& expected_type);
     void SetDeducedType(const ast::NodeBase& node, DeducedType&& deduced_type);
+    template <typename T>
+        requires std::derived_from<std::remove_cvref_t<T>, ast::NodeBase>
+    bool TrySetDeducedTypeFamily(const ast::NodeBase& node); // TODO: better name
     void CheckCompatibility(const ast::NodeBase& node, const ExpectedType& expected_type,
                             const DeducedType& deduced_type) const;
 
     NameContext name_context_;
     ast::AttributeStorage<ExpectedType, DeducedType> types_storage_;
 };
+
+template <typename T>
+    requires std::derived_from<std::remove_cvref_t<T>, ast::NodeBase>
+bool TypeChecker::TrySetDeducedTypeFamily(const ast::NodeBase& node) {
+    auto expected_type = types_storage_.tryGet<ExpectedType>(&node);
+    if (expected_type) {
+        return expected_type->CheckCompatibleWith<std::remove_cvref_t<T>>();
+    }
+    return true;
+}
 
 } // namespace typecheck
 } // namespace stella
